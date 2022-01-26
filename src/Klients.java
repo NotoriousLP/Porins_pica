93f48138874f7c0810cd342189378acd25764fdd
Porins <porinslauris@gmail.com>
2022-01-26 08:40:48 +0200

    Klasē klients pievienotas Metodes

--- src/Klients.java
+++ src/Klients.java
@@ -6,6 +6,8 @@ public class Klients extends Pica {
 	public String telefons;
 	public String Adrese;
 	public boolean vaiSutaMajas;
+	double majasPiegadesCena;
+	//Konstruktors
 	public Klients(String klientaVards, String klientaUzvards, String klientaTelefons, String klientaAdrese, boolean vaiKlientsSutaMajas, String picaTips, String picaPiedeva, double picaCena) {
 		super(picaTips,picaPiedeva,picaCena);
 		this.vards = klientaVards;
@@ -14,4 +16,18 @@ public class Klients extends Pica {
 		this.Adrese = klientaAdrese;
 		this.vaiSutaMajas = vaiKlientsSutaMajas;
 	}
+	//Metodes
+	public boolean vaiSutaMajas(boolean vaiSutaMajas) {
+		return vaiSutaMajas;
+	}
+	public String nosakaVardu(String vards) {
+		return vards;
+	}
+	public void jaSutaMajas(boolean vaiSutaMajas) {
+		if(vaiSutaMajas == true) {
+			 majasPiegadesCena = 5.73;
+		}
+	}
+	
+	
 }
