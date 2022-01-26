ad06d9dcadf8a95c55ac616425d5eed845a24925
Porins <porinslauris@gmail.com>
2022-01-26 08:27:52 +0200

    Klasē ieviestas Metodes

--- src/Pica.java
+++ src/Pica.java
@@ -1,4 +1,27 @@
 
-public class Pica {
-
+public class Pica{
+	//Atribūti
+	public String tips;
+	public String piedeva;
+	public double cena;
+	double skaits;
+	public Pica(String picaTips, String picaPiedeva, double picaCena) {
+		this.tips = picaTips;
+		this.piedeva = picaPiedeva;
+		this.cena = picaCena;
+	}
+	//Metodes
+	public String noteiktTipu(String tips) {
+		return tips;
+	}
+	public String noteiktPiedevu(String piedeva) {
+		return piedeva;
+	}
+	public double noteiktCenu(double cena) {
+		return cena;
+	}
+	public double vairakasPicas(double cena) {
+		skaits = skaits * cena;
+		return skaits;
+	}
 }
