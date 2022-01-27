67f56930862eb206c1b62c186672f09ca5d59808
Porins <porinslauris@gmail.com>
2022-01-27 14:40:54 +0200

    Klienta daļā ielikta jauna metode, kura izvada datus

--- src/Porins_pica/Klients.java
+++ src/Porins_pica/Klients.java
@@ -1,5 +1,7 @@
 package Porins_pica;
 
+import javax.swing.JOptionPane;
+
 public class Klients extends Pica {
 	//Atribūti
 	public String vards;
@@ -30,7 +32,16 @@ public class Klients extends Pica {
 		}
 		return majasPiegadesCena;
 	}
-
+	public void izvadit() {
+		JOptionPane.showMessageDialog(null, "Klienta vārds "+vards+"\n"
+				+ "Klienta uzvārds "+uzvards+"\n"
+				+ "Klienta telefona numurs "+telefons+"\n"
+				+ "Klienta adrese "+Adrese+"\n"
+				+ "Klienta picas tips "+tips+"\n"
+				+ "Picas garša "+garsa+"\n"
+				+ "Picas piedeva "+piedeva+"\n"
+				+ "Picas diametrs "+cm);
+	}
 	
 	
 }
